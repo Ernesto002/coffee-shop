9c4ad8fb4d0133338b14cb7657966c1800d8d31e
ernesto <ernesto002@icloud.com>
2021-03-28 17:17:59 -0700

    create views

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,13 @@
+class OrdersController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'orders/new'
+  get 'orders/show'
+  get 'orders/index'
+  get 'orders/edit'
+  get 'items/index'
+  get 'items/show'
+  get 'items/new'
+  get 'items/edit'
+  get 'customers/index'
+  get 'customers/show'
+  get 'customers/new'
+  get 'customers/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
